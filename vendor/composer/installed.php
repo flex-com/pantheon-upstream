94ec41744726422a59c5f454ad41c720ec1993ff
Cherie Jones <cherie@hellomonster.com>
2024-09-13 12:20:07 -0700

    Added upstream-management

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9bf0eee06b0b222647cfc00b93f5e4019314a7e0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9bf0eee06b0b222647cfc00b93f5e4019314a7e0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pantheon-systems/upstream-management' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '581530f69775cfd32acf944506de81510a28deca',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../pantheon-systems/upstream-management',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
